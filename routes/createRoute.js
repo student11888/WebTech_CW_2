b9b838c0ae322dc6bd365de5f06ca087d8d66e85
student11888 <wiut00011888@gmail.com>
2022-03-23 16:14:17 +0500

    Form validation implemented

--- routes/createRoute.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const createController = require('../controllers/createController');
-
-router.get('/create', createController.createController);
-
-module.exports = router;
